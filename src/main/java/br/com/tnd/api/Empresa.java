64962df4d4b404d3aa6dc497c14fc42634b2fbd1
Emanuel <emanuel.melechco@redetencia.com.br>
2018-05-25 13:34:22 -0400

    ec

---
+++ src/main/java/br/com/tnd/api/Empresa.java
@@ -0,0 +1,76 @@
+package br.com.tnd.api;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+	
+	public Empresa() {
+		
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	@PrePersist
+	public void antesPersistir() {
+		this.dataCriacao = new Date();
+		this.dataAtualizacao = new Date();
+	}
+
+	@PreUpdate
+	public void antesAtualizar() {
+		this.dataAtualizacao = new Date();
+	}
+}
---
+++ src/main/java/br/com/tnd/api/ImportacaoWebserviceApplication.java
@@ -0,0 +1,35 @@
+package br.com.tnd.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ImportacaoWebserviceApplication {
+
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ImportacaoWebserviceApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			Empresa empresa = new Empresa();
+			empresa.setRazaoSocial("Kazale IT");
+			empresa.setCnpj("01564564564564");
+			
+			this.empresaRepository.save(empresa);
+			
+			Empresa empresa1 = new Empresa();
+			empresa1.setRazaoSocial("Nutit");
+			empresa1.setCnpj("13213215348545");
+			
+			this.empresaRepository.save(empresa1);
+		};
+	}
+}
